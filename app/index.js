b104590dbc7516961716df3ebc75cb40c01fa3e4
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-03-11 10:59:51 -0300

    Fix eslint rules to reject semicolons

--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './actions/index'
+import './styles'
 
 ReactDOM.render(
     <h1>Hello World!</h1>,
     document.getElementById('app')
-);
+)
